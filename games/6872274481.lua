faad6e38a86a2d2756e28b426ae8d974fc97c3ee
sstvskids <staavstudios@gmail.com>
2025-09-17 19:55:44 +1000

    Update 6872274481.lua

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -8,6 +8,10 @@
 
 ]]
 
+local run = function(func)
+    local suc, res = pcall(func)
+    return suc == true and res or suc == false and ((writefile and writefile('lover.lua/errorlog.txt', res)) or warn(res))
+end
 local cloneref = cloneref or function(obj)
     return obj
 end
@@ -32,21 +36,37 @@ end
 local playersService = cloneref(game:GetService('Players'))
 local starterGui = cloneref(game:GetService('StarterGui'))
 local replicatedStorage = cloneref(game:GetService('ReplicatedStorage'))
+local runService = cloneref(game:GetService('RunService'))
+local lplr = playersService.LocalPlayer
 
 local main = Library.new()
 
-local tabs = {
+local remotes, tabs = {}, {
     Combat = main.create_tab('Combat'),
-    Blatant = main.create_tab('Blatant'),
+    Movement = main.create_tab('Movement'),
     Render = main.create_tab('Render'),
     Settings = main.create_tab('Settings')
 }
+
+local function isAlive(v)
+    if v.Character and v.Character:FindFirstChild('Humanoid') and v.Character:FindFirstChild('HumanoidRootPart') and v.Character.Humanoid.Health > 0 then
+        return true
+    end
+
+    return false
+end
+
+local function getPart(v)
+    if isAlive(v) then
+        return v.Character.PrimaryPart
+    end
+end
 
 run(function()
     local NetManaged = replicatedStorage.rbxts_include.node_modules["@rbxts"].net.out._NetManaged
     local BlockEngine = replicatedStorage.rbxts_include.node_modules["@easy-games"]["block-engine"].node_modules["@rbxts"].net.out._NetManaged
 
-    local Remotes = {
+    remotes = setmetatable({
         SwordHit = NetManaged.SwordHit,
         PickupItemDrop = NetManaged.PickupItemDrop,
         SetInvItem = NetManaged.SetInvItem,
@@ -55,5 +75,48 @@ run(function()
         SetObservedChest = NetManaged["Inventory/SetObservedChest"],
         PlaceBlock = BlockEngine.PlaceBlock,
         DamageBlock = BlockEngine.DamageBlock,
-    }
+    }, nil)
+end)
+
+run(function()
+    local Speed
+    local Value
+    tabs.Blatant.create_title({
+        name = 'Speed',
+        section = 'left'
+    })
+
+    Speed = tabs.Movement.create_toggle({
+        name = 'Speed',
+        flag = 'speed',
+
+        section = 'left',
+        enabled = false,
+
+        callback = function(callback)
+            if callback then
+                Library.connections.Speed = runService.PreSimulation:Connect(function()
+                    if isAlive(lplr) then
+                        lplr.Character.Humanoid.WalkSpeed = Value.value
+                    end
+                end)
+            else
+                if Library.connections.Speed then
+                    Library.connections.Speed:Disconnect()
+                    Library.connections.Speed = nil
+                end
+                lplr.Character.Humanoid.WalkSpeed = 16
+            end
+        end
+    })
+    Value = tabs.Blatant.create_slider({
+        name = 'Speed',
+        flag = 'speedslider',
+
+        section = 'left',
+
+        value = 23,
+        minimum_value = 16,
+        maximum_value = 23
+    })
 end)
